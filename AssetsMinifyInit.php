b2a7e60679ec1836d95f368c4c394b19b15fb22b
pputzer <pep-@users.noreply.github.com>
2015-10-10 11:12:32 +0200

    Set reasonable fonts_dir

--- AssetsMinifyInit.php
+++ AssetsMinifyInit.php
@@ -347,7 +347,8 @@ class AssetsMinifyInit {
 				if ( get_option('am_use_compass', 0) != 0 ) {
 					//Defines compass filter instance and sprite images paths
 					$compassInstance = new CompassFilter( get_option('am_compass_path', '/usr/bin/compass') );
-					$compassInstance->setImagesDir(get_theme_root() . "/" . get_template() . "/images");
+					$compassInstance->setImagesDir( get_theme_root() . "/" . get_template() . "/images" );
+					$compassInstance->setFontsDir( get_theme_root() . "/" . get_template() . "/fonts" );
 					$compassInstance->setGeneratedImagesPath( $this->assetsPath );
 					$compassInstance->setHttpGeneratedImagesPath( site_url() . str_replace( getcwd(), '', $this->assetsPath ) );
 					$compassInstance->setExternalEncoding('UTF-8');
